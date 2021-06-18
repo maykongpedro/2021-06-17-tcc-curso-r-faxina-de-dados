402662bf6805ce6874fdc243d7285e0cb3129129
maykongpedro <maykonglaffite@gmail.com>
2021-06-18 01:57:02 +0000

    add organização da primeira tabela

---
+++ data-raw/02-Mapeamentos-SFB-IFPR.R
@@ -0,0 +1,84 @@
+
+
+# Carregar e instalar pacotes necessários ---------------------------------
+if(!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, janitor, usethis, pdftools, tabulizer)
+
+# Carregar apenas pipe
+'%>%' <- magrittr::`%>%`
+
+
+# Testando com mapeamento do PR -------------------------------------------
+
+# ver páginas
+url_mapeamento <- "./data-raw/pdf/IFPR e SFB – Mapeamento dos plantios florestais do estado do Paraná.pdf"
+pdftools::pdf_convert(url_mapeamento, pages = 30, filenames = "./inst/ifpr_pag30.png")
+
+# extrair tabela
+tabela_ext <- tabulizer::extract_tables(url_mapeamento, pages = 30)
+
+
+# ajustar dados - all
+tabela_tidy <-
+  tabela_ext %>% 
+  # selecionar o item 1 da lista
+  purrr::pluck(1) %>% 
+  
+  # transformar em tible 
+  tibble::as_tibble(.name_repair = "unique") %>%
+  
+  # setar os nomes das colunas
+  purrr::set_names(c("regiao", "nucleo_regional", "corte", "eucalipto_pinus", "total", "percentual")) %>% 
+  
+  # retirar linhas iniciais e finais
+  dplyr::slice(-c(1:3), 
+               -c(36, 37)) %>% 
+  
+  # substituir o que é vazio por NA
+  dplyr::mutate(dplyr::across(dplyr::everything(),
+                              dplyr::na_if, "")) %>% 
+  
+  # adicionar e modificar colunas
+  dplyr::mutate(
+    
+    # adicionar coluna de índice
+    id = dplyr::row_number(),
+    
+    # retirar as regiões existentes
+    regiao = dplyr::case_when(regiao != NA ~ ""),
+    
+    # colocar o nome da região na primeira linha de cada nucleo_regional
+    regiao = dplyr::case_when(id == 1 ~ "Centro-Oeste",
+                              id == 4 ~ "Centro-Sul",
+                              id == 13 ~ "Litoral",
+                              id == 16 ~ "Noroeste",
+                              id == 21 ~ "Norte",
+                              id == 28 ~ "Oeste",
+                              TRUE ~ regiao)) %>% 
+  
+  # preencher regiões com base na primeira linha de cada item
+  tidyr::fill(regiao) %>% 
+  
+  # retirar linhas com NA
+  tidyr::drop_na() %>% 
+  
+  # retirar coluna de ID
+  dplyr::select(-id)
+  
+
+# Verificar no console
+tabela_tidy %>% 
+  print(n = nrow(tabela_tidy))
+
+
+# Separar coluna de gênero
+
+
+
+
+
+
+
+
+# Deletar arquivos temporários --------------------------------------------
+fs::file_delete("./inst/ifpr_pag30.png")
