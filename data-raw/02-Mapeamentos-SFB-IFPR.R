d6de8820883348fcf22e1457e9cf2b9e5f4a3b2c
maykongpedro <maykonglaffite@gmail.com>
2021-06-19 20:51:58 +0000

    add imagens de cada página e estrutura das tabelas posteriores

--- data-raw/02-Mapeamentos-SFB-IFPR.R
+++ data-raw/02-Mapeamentos-SFB-IFPR.R
@@ -8,23 +8,50 @@ pacman::p_load(tidyverse, janitor, usethis, pdftools, tabulizer)
 '%>%' <- magrittr::`%>%`
 
 
-
 # Visualizar pdf ----------------------------------------------------------
 
-# ver páginas
-url_mapeamento <- "./data-raw/pdf/IFPR e SFB – Mapeamento dos plantios florestais do estado do Paraná.pdf"
-pdftools::pdf_convert(url_mapeamento, pages = 30, filenames = "./inst/ifpr_pag30.png")
+# caminho do pdf
+url_mapeamento <- "./data-raw/pdf/01-SFB-IFPR/IFPR e SFB – Mapeamento dos plantios florestais do estado do Paraná.pdf"
+
+# definir páginas para extração
+paginas_alvo <- c(30, 36, 37, 40:72)
+
+# definir caminho e nomes para cada arquivo imagem
+nome_arquivos <- paste0("./inst/01-PR-SFB-IFPR/" , paginas_alvo, ".png")
+
+# salvar cada página para visualizar
+pdftools::pdf_convert(url_mapeamento, 
+                      pages = paginas_alvo,
+                      filenames = nome_arquivos)
+
+# Podemos perceber que tem algumas tabelas que são divididas entre páginas, e
+# algumas que estão em páginas que contém um mapa (imagem) na mesma página.
+# Essas últimas dão problema na função extract_tables do Tabulizer, não consegui
+# arrumar e nem entender o porquê. 
+# Para contornar isso, fiz a extração das páginas que tem imagens e tabelas,
+# converti para word e retirei as imagens dessas páginas, após isso, salvei
+# novamente as páginas em pdf, assim gerando um arquivo adicional com todas
+# as páginas que continham tabela e imagem na mesma folha porém agora somente
+# com a tabela.
+
+# esse é um exeplo do erro disparado porque tem uma imagem na página
+tabela_2_campo_mourao <- tabulizer::extract_tables(url_mapeamento, pages = 36)
+
+# até é possível extrair apenas o texto, porém o trabalho para faxinar seria imenso
+tabulizer::extract_text(url_mapeamento, pages = 36)
+
+# visualizar uma imagem
+# OpenImageR::imageShow("./inst/30.png")
+
+
+# Tabela 1- Extrair e transformar dados -----------------------------------
 
 # extrair tabela
-tabela_ext <- tabulizer::extract_tables(url_mapeamento, pages = 30)
-
-
-
-# Transformar dados -------------------------------------------------------
+tabela_1_total <- tabulizer::extract_tables(url_mapeamento, pages = 30)
 
 # ajustar dados - all - tabela de área total
 tabela_arrumada <-
-  tabela_ext %>% 
+  tabela_1_total %>% 
   # selecionar o item 1 da lista
   purrr::pluck(1) %>% 
   
@@ -108,6 +135,77 @@ tabela_tidy <-
     )
 
 print(tabela_tidy)
+
+
+# Tabela 2 - Extrair e transformar dados ----------------------------------
+
+
+paginas_processadas <- "./data-raw/pdf/01-SFB-IFPR/IFPR e SFB-páginas-36,40,42,44-46,48-49,51-53,55,57-58,60-61,63-64,66,68-69,71-tabelas.pdf"
+
+tabelas_pag_com_imgs <-
+  tabulizer::extract_tables(paginas_processadas,
+                            method = "stream")
+
+print(tabelas_pag_com_imgs)
+
+
+# Páginas apenas com tabelas as quais foram identificadas corretamente
+paginas_tabelas <- c(37, 41, 43, 56, 59, 65, 67, 72)
+nucleos_regionais_tab <- c("Campo Mourão",
+                           "Curitiba",
+                           "Guarapuava",
+                           "Umuarama",
+                           "Cornélio Procópio",
+                           "Maringá",
+                           "Cascavel",
+                           "Toledo"
+                           )
+
+tabelas_pag_sem_imgs <- tabulizer::extract_tables(url_mapeamento,
+                                                  pages = paginas_tabelas,
+                                                  method = "stream")
+# Nomeando cada uma das listas
+tabelas_pag_sem_imgs_nomeadas <- list(
+  Campo_Mourao = tabelas_pag_sem_imgs[[1]],
+  Curitiba = tabelas_pag_sem_imgs[[2]],
+  Guarapuava =  tabelas_pag_sem_imgs[[3]],
+  Umuarama = tabelas_pag_sem_imgs[[4]],
+  Cornelio_Procopio = tabelas_pag_sem_imgs[[5]],
+  Maringa = tabelas_pag_sem_imgs[[6]],
+  Cascavel = tabelas_pag_sem_imgs[[7]],
+  Toledo = tabelas_pag_sem_imgs[[8]]
+)
+
+tabelas_pag_sem_imgs_nomeadas
+print(tabelas_pag_sem_imgs)
+
+
+
+
+
+paginas_tabelas_semi_ident <- c(47, 50, 54, 62)
+nucleos_regionais_tab_semi_ident <- c("Pato Branco",
+                                      "União da Vitória",
+                                      "Paranavaí",
+                                      "Jacarezinho")
+
+
+
+
+# Francisco Beltrão (pág 70) não quis funcionar. Foi necessário extrair
+# a página, converter para word e salvar novamente em pdf
+
+# Não captura nada
+tabela_fb <- tabulizer::extract_tables(url_mapeamento,
+                                       pages = 70)
+
+# Arquivo processado apenas com a página 70
+pag70 <- "./data-raw/pdf/01-SFB-IFPR/IFPR e SFB-70-tabela.pdf"
+tabela_fb <- tabulizer::extract_tables(pag70,
+                                       pages = 1)
+print(tabela_fb)
+
+
 
 
 
