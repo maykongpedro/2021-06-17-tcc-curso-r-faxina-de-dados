45731d990cc3133d6799024bcf55f3dca6b8de54
maykongpedro <maykonglaffite@gmail.com>
2021-06-21 00:50:50 +0000

    add extraçao das páginas com imagens e fórmula genérica

--- data-raw/02-Mapeamentos-SFB-IFPR.R
+++ data-raw/02-Mapeamentos-SFB-IFPR.R
@@ -125,9 +125,9 @@ tabela_tidy <-
 
 tibble::view(tabela_tidy)
 
-# Como a próxima etapa é transformar algumas coluans em linhas, acho desnecessário
+# Como a próxima etapa é transformar algumas colunas em linhas, acho desnecessário
 # manter a coluna de percentual e total, pois ambas podem ser obtida facilmente 
-# por meio de funções do tidyverse, além de poderem ocasioanr confusões ao longo
+# por meio de funções do tidyverse, além de poderem ocasionar confusões ao longo
 # de análises
 tabela_tidy <- 
   tabela_tidy %>% 
@@ -184,6 +184,27 @@ names(tabelas_pag_com_imgs) <- nucleos_regionais_tab_imgs
 # Printar no console
 print(tabelas_pag_com_imgs)
 
+
+# Extração genérica
+tb_campo_mourao_a <-
+  tabelas_pag_com_imgs %>% 
+  purrr::pluck("Campo Mourão") %>% 
+  tibble::as_tibble(.name_repair = "unique") %>%
+  purrr::set_names(c("municipio", "corte", "eucalipto", "pinus", "total", "percentual")) %>% 
+  dplyr::slice(-c(1:3)) %>% 
+  dplyr::mutate(dplyr::across(dplyr::everything(),
+                              dplyr::na_if, "")) %>% 
+  dplyr::select(-total, -percentual) %>% 
+  dplyr::mutate(tabela_fonte = paste0("Área de Plantio de ",
+                                      names(tabelas_pag_com_imgs[1])),
+                nucleo_regional = names(tabelas_pag_com_imgs[1])) %>% 
+  dplyr::relocate(tabela_fonte:nucleo_regional, .before = municipio) 
+  
+
+print(tb_campo_mourao_a, n =50)
+
+
+faxinar_tabela_ng_pag_com_img(tabelas_pag_com_imgs, nucleos_regionais_tab_imgs[1])
 
 
 
