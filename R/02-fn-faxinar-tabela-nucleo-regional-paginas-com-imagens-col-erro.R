14ce20295a37ffd8837805d121d9aebd9fb3226c
maykongpedro <maykonglaffite@gmail.com>
2021-06-25 00:44:32 +0000

    ajustando erros nas tabelas com imagens (coluna vazia)

---
+++ R/02-fn-faxinar-tabela-nucleo-regional-paginas-com-imagens-col-erro.R
@@ -0,0 +1,43 @@
+#' faxinar_tabela_ng_pag_com_img_col_erro
+#'
+#' Descrição da função
+#' Função utilizada para faxinar as tabelas de núcleos regionais das páginas do
+#' mapeamento SFB-IFPR, deixando elas estruturadas de uma maneira prática e utilizável.
+#' O conceito de utilização como base de dados se estrutura no conceito de "tidy".
+#' 
+#' Essa função foi especificamente ajustada para as páginas que continham tabelas
+#' e imagens, cuja extração resultou em tabelas com 6 colunas e uma delas em branco.
+#' 
+#' @param tabela_bruta_extraida tabela extraida do pdf pela função tabulizer::extract_tables
+#' @param nome_nucleo_regional nome do núcleo regional
+#' 
+#' @return retorna uma tibble organizada e faxinada com as informações das tabelas das páginas
+#' 
+faxinar_tabela_ng_pag_com_img_col_erro <- function(tabela_bruta_extraida, nome_nucleo_regional){
+  
+  loc <- readr::locale(decimal_mark = ",", grouping_mark = ".")
+  
+    tabela_bruta_extraida %>%
+      purrr::pluck(nome_nucleo_regional) %>%
+      tibble::as_tibble(.name_repair = "unique") %>%
+      purrr::set_names(c("municipio", "corte", "eucalipto_pinus", "erro", "total", "percentual")) %>%
+      dplyr::slice(-c(1:3)) %>%
+      dplyr::mutate(dplyr::across(dplyr::everything(),
+                                  dplyr::na_if, "")) %>%
+      dplyr::select(-erro, -total, -percentual) %>%
+      dplyr::mutate(tabela_fonte = paste0("Área de Plantio de ",
+                                          nome_nucleo_regional),
+                    nucleo_regional = nome_nucleo_regional) %>%
+      dplyr::relocate(tabela_fonte:nucleo_regional, .before = municipio) %>%
+      tidyr::separate(col = eucalipto_pinus,
+                      sep = " ",
+                      into = c("eucalipto", "pinus")) %>%
+      dplyr::mutate(dplyr::across(.cols = corte:pinus,
+                                  readr::parse_number, locale = loc)) %>%
+      dplyr::filter(!municipio %in% c("TOTAL", "%")) %>% 
+      tidyr::pivot_longer(cols = corte:pinus,
+                          names_to = "tipo_genero",
+                          values_to = "area_ha")
+    
+  
+}
