fb0c3c63fbbe95762c3efcc0bb51335f0fba09b3
maykongpedro <maykonglaffite@gmail.com>
2021-06-26 15:40:18 +0000

    add base completa com o código de município do IBGE

--- data-raw/09-Adiciona-Coluna-Cod-IBGE.R
+++ data-raw/09-Adiciona-Coluna-Cod-IBGE.R
@@ -25,7 +25,7 @@ base_muni_pr <- geobr::read_municipality(code_muni= "PR", year=2010)
 # transformar coluna de municípios
 base_alterada <-
   base_mapeamento %>% 
-  dplyr::mutate(municipio = stringr::str_to_lower(municipio))
+  dplyr::mutate(name_muni = stringr::str_to_lower(municipio))
 
 
 # fazendo primeiro join
@@ -33,7 +33,6 @@ base_geo <-
   base_muni_pr %>% 
   dplyr::mutate(municipio = stringr::str_to_lower(name_muni)) %>% 
   dplyr::left_join(base_alterada, by = "municipio") 
-  #dplyr::filter(is.na(tipo_genero))
 
 
 # identificando itens faltantas
@@ -41,41 +40,56 @@ base_geo %>%
   dplyr::filter(is.na(tipo_genero))
 
 
-# fazendo segundo join considerando esses itens
+
+# fazendo segundo join considerando municípios não encontrados
 base_geo_ajust <-
   base_muni_pr %>% 
   dplyr::mutate(
     name_muni = dplyr::case_when(name_muni == "Altônia" ~ "Altonia",
                                  name_muni == "Flor Da Serra Do Sul" ~ "Flor da Serra Azul",
-                                 name_muni == "Itapejara D'oeste" ~ "Flor da Serra Azul",
+                                 name_muni == "Itapejara D'oeste" ~ "Itapejara D' Oeste",
                                  name_muni == "Joaquim Távora" ~ "Joaquim Távola",
-                                 name_muni == "Pérola D'oeste" ~ "Pérola D'Oeste",                                
+                                 name_muni == "Pérola D'oeste" ~ "Pérola D' Oeste",                                
                                  name_muni == "Quitandinha" ~ "Quitandinhas",
                                  name_muni == "Santa Lúcia" ~ "Santa Lucia",
                                  name_muni == "Santa Tereza Do Oeste" ~ "Santa Terezinha do Oeste",
                                  name_muni == "Santa Terezinha De Itaipu" ~ "Santa Terezinha do Itaipu",
+                                 name_muni == "Santo Antônio Do Paraíso" ~ "Santo Antônio do Paraiso",
+                                 name_muni == "São José Dos Pinhais" ~ "São José dos Pinhas",
                                  TRUE ~ name_muni),
     
-    municipio = stringr::str_to_lower(name_muni)
+    name_muni = stringr::str_to_lower(name_muni)
   ) %>% 
-  dplyr::left_join(base_alterada, by = "municipio") %>% 
-  dplyr::filter(is.na(tipo_genero))
+  dplyr::left_join(base_alterada, by = "name_muni") 
+
   
+# verificando itens não encontrados
+base_geo_ajust %>% 
+  dplyr::filter(is.na(tipo_genero))
+
+# o único que não foi encontrado é Guaraqueçaba, que reealmente não existe mapeamento
+# na base para esse município
 
 
+# adicionar na base de mapeamento o código do IBGE
+base_mapeamento
+
+base_mapeamento_com_cod_ibge <-
+  base_mapeamento %>% 
+  dplyr::mutate(
+    name_muni = stringr::str_to_lower(municipio)
+  ) %>% 
+  dplyr::left_join(base_geo_ajust) %>% 
+  dplyr::select(-name_muni, -code_state, -abbrev_state, -geom) %>% 
+  dplyr::relocate(code_muni, .after = "municipio")
+  
+# verificando itens não encontrados
+base_mapeamento_com_cod_ibge %>% 
+  dplyr::filter(is.na(code_muni))
 
 
-# fazendo join considerando os municípios não encontrados
-"Altônia" = "Altonia"
-"Flor Da Serra Do Sul" = "Flor da Serra Azul",
-"Guaraqueçaba" = "Guaraqueçaba" -> nao tem mesmo
-"Itapejara D'oeste" = "Itapejara D' Oeste"
-"Joaquim Távola" = "Joaquim Távola"
-"Pérola D'oeste" = "Pérola D' Oeste"
-"Quitandinha" = "Quitandinhas"
-"Santa Lúcia" = "Santa Lucia"
-"Santa Tereza Do Oeste" = "Santa Terezinha do Oeste"
-"Santa Terezinha De Itaipu" =  "Santa Terezinha do Itaipu"
+# Salvar base com o código ibge -------------------------------------------
+saveRDS(base_mapeamento_com_cod_ibge, "./data/mapeamento_SFB-IFPR_completo_cod_IBGE.rds")
 
 
 
